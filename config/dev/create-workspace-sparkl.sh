b3ad7b276053856317165d8cf3d03cfafb3680ac
Diethard Steiner <diethard.steiner@gmail.com>
2014-08-24 17:30:13 +0100

    reorganized folder structure

--- config/dev/create-workspace-sparkl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# to see all gnome-terminal options run gnome-terminal --help-all
-
-gnome-terminal --tab --title biserver --working-directory /home/dsteiner/apps/pentaho/biserver/biserver-ce-5.1.0 --tab --title sql --working-directory=/home/dsteiner/apps/sqlworkbench --tab --title pdi --working-directory /home/dsteiner/apps/pentaho/pdi/pdi-ce-5.1.0.0 --tab --title git --working-directory /home/dsteiner/projects/bissolTableDataEditor --tab --title netbeans --working-directory /home/dsteiner/apps/netbeans-8.0/bin --execute ./netbeans
-
-#gnome-terminal --tab --title sql --working-directory=/home/dsteiner/apps/sqlworkbench --execute sh ./sqlworkbench.sh --tab --title=pdi --working-directory=/home/dsteiner/apps/pentaho/pdi/pdi-ce-5.1.0.0 --execute sh ./spoon.sh --tab --title=git --working-directory=/home/dsteiner/Dropbox/projects/bissolTableDataEditor --tab --title biserver --working-directory=/home/dsteiner/apps/pentaho/biserver/biserver-ce-5.1.0 --execute sh ./start-pentaho.sh
